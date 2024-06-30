4f7aa6fa66242739ff4ad752687d8b751489a0ab
capablanca42 <jandrypalacios30@gmail.com>
2024-06-30 11:33:21 -0500

    funcion  factorial

---
+++ eje01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+string ecuacioncuadratica(double a, double b, double c, double x1, double x2)
+{ 
+    if (a == 0)
+ return "error de division por cero";
+}
+
+
+int main ()
+{
+
+
+
+
+    return 0;
+}
---
+++ factorrecursivo.cpp
@@ -0,0 +1,46 @@
+/*
+fecha: 2024 -06 -30
+autor:jandry zambrano
+tema: funcionnes recursivas 
+- Deben tener un punto de salida, parano crear bucle infinito
+-Las funciones recursivas reemplazan a los bucles 
+*/
+ #include<iostream>
+ using namespace std;
+  int factorial1(int num)
+  {
+
+if( num==0 || num==1  ):
+return 1;
+else
+{
+
+for(int k=1; k<=num; k++)
+fac=fac * k
+return fac;
+
+}
+
+
+
+
+//calcular el factorial recursivididad
+int factorialrecursivo(int mun)
+{
+    if(num==0 || num==1)
+    return 1;
+}
+
+  }
+   int main()
+   {
+cout<<endl<<"Factorial de  5 es"<<factorialrecursivo(5);
+cout<<endl<<"Factorial de  5 es"<<factorialrecursivo(6);
+cout<<endl<<"Factorial de  5 es"<<factorialrecursivo(5);
+cout<<endl<<"Factorial de  5 es"<<factorialrecursivo(6);
+
+
+
+
+    return 0;
+   }
